636716312782e5dd531c59407da76325915cdced
S-C-M <castanedasergio321@gmail.com>
2016-04-19 21:16:11 +0000

    Done.

--- db/migrate/20141022163315_create_tweets.rb
+++ db/migrate/20141022163315_create_tweets.rb
@@ -1,12 +1,3 @@
 class CreateTweets < ActiveRecord::Migration
-  def up
-    create_table :tweets do |t| 
-      t.string :username 
-      t.string :status
-    end
-  end
-  
-  def down
-    drop_table :tweets
-  end
+   belongs_to :user
 end
